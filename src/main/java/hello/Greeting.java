ecdb69561720029d22021ce512fe59c6cb34e4a1
Stevo5o <stevo.joc@gmail.com>
2015-10-12 14:28:45 +0100

    HTML :sushi:

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -1,28 +1,24 @@
 package hello;
-//The implementation of the method body relies on a view technology, 
-//in this case Thymeleaf, to perform server-side rendering of the HTML. 
-public class Greeting
-{
-	private long id;
-	private String content;
 
-	public long getId()
-	{
-		return id;
-	}
+public class Greeting {
 
-	public void setId(long id)
-	{
-		this.id = id;
-	}
+    private long id;
+    private String content;
 
-	public String getContent()
-	{
-		return content;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setContent(String content)
-	{
-		this.content = content;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
 }
